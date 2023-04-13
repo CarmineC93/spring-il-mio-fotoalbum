f852809fe8d1ab3a598040b5e9db9ac4b4ca3f77
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2023-04-13 13:08:58 +0200

    testing index in controller: OK

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+
+INSERT INTO photos (title, description, visible, url) VALUES('Photo 1', 'la foto number 1', 1, 'https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg');
