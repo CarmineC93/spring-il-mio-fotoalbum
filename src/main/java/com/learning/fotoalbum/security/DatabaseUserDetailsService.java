f113d0dfd583a34c1e4394542464bf2ba57b3841
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2023-04-14 14:44:15 +0200

    add an interface and a service class to link with database

---
+++ src/main/java/com/learning/fotoalbum/security/DatabaseUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.learning.fotoalbum.security;
+
+import com.learning.fotoalbum.model.User;
+import com.learning.fotoalbum.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
+
+public class DatabaseUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override                                     //email
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByEmail(username);
+        if ( user.isPresent()){
+            return new DatabaseUserDetails(user.get());
+        }else{
+            throw new UsernameNotFoundException("user with this username: " + username + " not found");
+        }
+     }
+}
